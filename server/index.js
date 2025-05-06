6a407063676705f6b1882490d26a9fcd936dba0e
fagojs <sushil.phago123@gmail.com>
2025-05-06 16:38:24 +1000

    update: added module for database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const connectDB = require("./database/connection");
+
 dotenv.config()
 
 const app = express();
@@ -8,10 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Server is running...");
-});
+// app.get("/", (req,res)=>{
+//     res.send("Server is running...");
+// });
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at PORT: ${PORT}`);
 });
+
+connectDB()
