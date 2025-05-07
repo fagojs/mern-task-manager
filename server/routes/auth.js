6fe70c80312aa5ef5adca536d158d035aacb7526
fagojs <sushil.phago123@gmail.com>
2025-05-07 15:49:19 +1000

    feat(auth): implement register/login with jwt and password hashing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,63 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+const User = require("../models/User")
+
+router.post("/register", async (req, res) => {
     
-    // extract user data from request body
+    const {username, email, password} = req.body;
 
-    // Hash the password
-    // create new user in the database
-    // save user to the database
+    const hashPassword = await bcrypt.hashSync(password, 10);
 
+    const newUser = new User({
+        username: username,
+        email: email,
+        password: hashPassword
+    })
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json({message: "User added successfully", user: savedUser});
+    } catch (error) {
+        res.status(400).json({message:"Server error", error: error.message});
+    }
 
 });
 
-router.post("/login", (req, res) => {
-    // extract user data from request body
- 
-    // verify the password
-    // if password is correct, send success response with jwt token
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne(email);
+    
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Invalid password"});
+        }
+    
+        const token = jwt.sign(
+            {id: user._id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        );
+    
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            },
+        });        
+    } catch (error) {
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+
 });
