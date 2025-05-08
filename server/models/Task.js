f1c5f88574797268ee8119f3ed6ee55b8c8b6298
fagojs <sushil.phago123@gmail.com>
2025-05-08 14:49:02 +1000

    feat(task): export task model

--- server/models/Task.js
+++ server/models/Task.js
@@ -11,3 +11,5 @@ const taskSchema = new mongoose.Schema({
     },
     user: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
 }, {timestamps: true})
+
+module.exports = mongoose.model("Task", taskSchema);
