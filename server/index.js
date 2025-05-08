960a4b2e1cfc8d2ea052b5724500d6e59d1847d2
fagojs <sushil.phago123@gmail.com>
2025-05-08 15:25:28 +1000

    feat(task): add routes for tasks

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 
 const connectDB = require("./database/connection");
 const userRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/task");
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/user", userRoutes)
+app.use("/tasks", taskRoutes)
 
 
 app.listen(PORT, ()=>{
