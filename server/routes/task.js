e5d05af4625385c1fc239a640b04419386ca3b2c
fagojs <sushil.phago123@gmail.com>
2025-05-09 15:30:23 +1000

    refactor(task): add controller for clean and maintainable logic

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,86 +1,17 @@
 const express = require("express");
 
-const Task = require("../models/Task");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/", auth, async (req, res)=> {
-    try {
-        const allTasks = await Task.find({user: req.user.id}).sort({ createdAt: -1 });
-        res.status(200).json({message: "Task retrieved successfully", tasks: allTasks});
-    } catch (error) {
-        res.status(401).json({message: "Server error", error: error.message});
-    }
-});
+const {getAllTasks, createTask, updateTask, deleteTask} = require("../controllers/taskController");
 
-router.post("/add", auth, async (req, res) => {
-    const {title, description, dueDate, status} = req.body;
+router.get("/", auth, getAllTasks);
 
-    try {
-        const newTask = new Task({
-            title: title,
-            description: description,
-            dueDate: dueDate,
-            status: status,
-            user: req.user.id
-        });
+router.post("/add", auth, createTask);
 
-        const savedTask = await newTask.save();
-        res.status(201).json({
-            message: "New task added successfully",
-            task: savedTask,
-        });
-    } catch (error) {
-        res.status(400).json({message: "Server error", error: error.message});
-    }
-});
+router.put("/update/:id", auth, updateTask);
 
-router.put("/update/:id", auth, async (req, res)=>{
-    const {id} = req.params;
-    const {newTitle, newDescription, newDueDate, newStatus} = req.body;
-
-    try {
-        const task = await Task.findById({_id: id});
-
-        if (!task) {
-            return res.status(404).json({ message: "Task not found" });
-        }
-
-        if (task.user.toString() !== req.user.id) {
-            return res.status(403).json({ message: "You are not authorized to update this task" });
-        }
-        task.title = newTitle || task.title;
-        task.description = newDescription || task.description;
-        task.dueDate = newDueDate || task.dueDate;
-        task.status = newStatus || task.status;
-
-        const updatedTask = await task.save()
-        res.status(200).json({
-            message: "Task updated successfully",
-            task: updatedTask,
-        });
-    } catch (error) {
-        res.status(401).json({ message: "Server error", error: error.message });
-    }
-})
-
-router.delete("/delete/:id", auth, async (req, res)=>{
-    const {id} = req.params;
-    try {
-        const task = await Task.findById(id);
-        if (!task) {
-            return res.status(404).json({ message: "Task not found" });
-        }
-        if (task.user.toString() !== req.user.id) {
-            return res.status(403).json({ message: "You are not authorized to remove this task" });
-        }
-
-        await task.deleteOne();
-        res.status(200).json({message: "Task deleted successfully"});
-    } catch (error) {
-        res.status(401).json({ message: "Server error", error: error.message });
-    }
-})
+router.delete("/delete/:id", auth, deleteTask);
 
 module.exports = router;
